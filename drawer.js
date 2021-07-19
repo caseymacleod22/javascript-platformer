d5c15817a419b770a03614edb0cce4b0936bec95
Casey MacLeod <caseymacleod22@gmail.com>
2021-07-19 11:39:37 -0400

    adds circle and line functions

--- drawer.js
+++ drawer.js
@@ -19,11 +19,34 @@ function strokeColor(r, g, b) {
     render.strokeStyle = 'rgb('+r+','+g+','+b+')'
 }
 
+function noStroke() {
+    render.strokeStyle = 'rgba(0, 0, 0, 0)'
+}
+
 function strokeSize(size) {
     render.lineWidth = String(size)
+}
+
+function noFill() {
+    render.fillStyle = 'rgba(0, 0, 0, 0)'
 }
 
 function background(r, g, b) {
     fillColor(r, g, b)
     rectangle(0, 0, width, height)
 }
+
+function line(x1, y1, x2, y2) {
+    render.beginPath()
+    render.moveTo(x1, y1)
+    render.lineTo(x2, y2)
+    render.stroke()
+}
+
+function circle(x, y, r) {
+    render.beginPath()
+    render.arc(x, y, r, 0, 2*Math.PI)
+    render.fill()
+    render.stroke()
+}
+
--- main.js
+++ main.js
@@ -14,9 +14,14 @@ window.onresize = function() {
 
 
     background(25, 25, 25)
-    fillColor(255, 0, 125)
+    noFill()
     strokeColor(0, 255, 255)
     strokeSize(3)
     rectangle(25, 25, 50, 100)
+    strokeColor(255, 0, 0)
+    line(50, 100, 25, 300)
+    noStroke()
+    fillColor(0, 255, 255)
+    circle(200, 200, 75, 67)
 }
 window.onresize()
