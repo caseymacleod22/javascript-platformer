f4d58749810c3377e4b7c998c920ad87888ae319
Casey MacLeod <caseymacleod22@gmail.com>
2021-07-19 11:25:48 -0400

    adds background and rectangle, window dynamically resizes

--- drawer.js
+++ drawer.js
@@ -4,3 +4,26 @@ function rectangle(x, y, w, h) {
     render.fill()
     render.stroke()
 }
+
+function fillColor(r, g, b) {
+    r = String(r)
+    g = String(g)
+    b = String(b)
+    render.fillStyle = 'rgb('+r+','+g+','+b+')'
+}
+
+function strokeColor(r, g, b) {
+    r = String(r)
+    g = String(g)
+    b = String(b)
+    render.strokeStyle = 'rgb('+r+','+g+','+b+')'
+}
+
+function strokeSize(size) {
+    render.lineWidth = String(size)
+}
+
+function background(r, g, b) {
+    fillColor(r, g, b)
+    rectangle(0, 0, width, height)
+}
--- main.js
+++ main.js
@@ -1,2 +1,22 @@
 let canvas = document.getElementById('mycanvas')
 let render = canvas.getContext('2d')
+let width
+let height
+canvas.style.left = '0px'
+canvas.style.top = '0px'
+canvas.style.position = 'absolute'
+
+window.onresize = function() {
+    width = window.innerWidth
+    height = window.innerHeight
+    canvas.width = width
+    canvas.height = height
+
+
+    background(25, 25, 25)
+    fillColor(255, 0, 125)
+    strokeColor(0, 255, 255)
+    strokeSize(3)
+    rectangle(25, 25, 50, 100)
+}
+window.onresize()
