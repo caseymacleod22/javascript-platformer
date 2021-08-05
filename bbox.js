805937705745c84df7148f7d01029e244e29cc32
Casey MacLeod <caseymacleod22@gmail.com>
2021-08-04 23:06:43 -0400

    game state updates

--- bbox.js
+++ bbox.js
@@ -7,12 +7,12 @@ class bbox {
         this.x2 = this.x1 + this.w
         this.y2 = this.y1 + this.h
     }
-    // update(x, y) {
-    //     this.x1 = x
-    //     this.y1 = y
-    //     this.x2 = x + this.w
-    //     this.y2 = y + this.h
-    // }
+    update(x, y) {
+        this.x1 = x
+        this.y1 = y
+        this.x2 = x + this.w
+        this.y2 = y + this.h
+    }
     collision(bbx) {
         return (this.x1 < bbx.x2 && this.x2 > bbx.x1 && this.y1 < bbx.y2 && this.y2 > bbx.y1)
     }
