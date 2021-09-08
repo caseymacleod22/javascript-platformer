fe9c35311fcc962a071374f20b4d4092ecdb32ca
Casey MacLeod <caseymacleod22@gmail.com>
2021-09-07 22:59:58 -0400

    mouse x and y coordinate variables

--- keys.js
+++ keys.js
@@ -1,9 +1,9 @@
-// let mouseX = 0
-// let mouseY = 0
-// document.onmousemove = function(event) {
-//     mouseX = event.clientX
-//     mouseY = event.clientY
-// }
+let mouseX = 0
+let mouseY = 0
+document.onmousemove = function(event) {
+    mouseX = event.clientX
+    mouseY = event.clientY
+}
 
 
 let keyPress = {}
